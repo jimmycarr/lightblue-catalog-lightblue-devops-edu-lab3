8a86177008189d4d47f02470fe4463d9461418f8
James Carr <jamescarr@uk.ibm.com>
2018-01-31 18:13:27 +0000

    Fixed file structure to match "final" code

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -1,19 +1,17 @@
 package catalog;
 
+import catalog.models.*;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import catalog.models.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-
 
 /**
  * REST Controller to manage Inventory database
@@ -22,23 +20,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CatalogController {
 
     Logger logger = LoggerFactory.getLogger(CatalogController.class);
-    
-    
- @Autowired
- InventoryRepo itemsRepo;
- 
-  @RequestMapping(value = "/items", method = RequestMethod.GET)
-  @ResponseBody
-  Iterable<Inventory> getInventory() {
-      return itemsRepo.findAll();
-  }
 
- @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
-  ResponseEntity<?> getById(@PathVariable long id) {
-      if (!itemsRepo.exists(id)) {
-          return ResponseEntity.notFound().build();
-      }
-      return ResponseEntity.ok(itemsRepo.findOne(id));
-  }
+    @Autowired
+    InventoryRepo itemsRepo;
+
+    /**
+     * @return all items in inventory
+     */
+    @RequestMapping(value = "/items", method = RequestMethod.GET)
+    @ResponseBody
+    Iterable<Inventory> getInventory() {
+        return itemsRepo.findAll();
+    }
+
+    /**
+     * @return item by id
+     */
+    @RequestMapping(value = "/items/{id}", method = RequestMethod.GET)
+    ResponseEntity<?> getById(@PathVariable long id) {
+                if (!itemsRepo.exists(id)) {
+                        return ResponseEntity.notFound().build();
+                }
+
+                return ResponseEntity.ok(itemsRepo.findOne(id));
+    }
 
 }
