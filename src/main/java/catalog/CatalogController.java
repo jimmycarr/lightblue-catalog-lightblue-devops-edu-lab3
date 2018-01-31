cae0ec1faaf4ca89b68ed4265ea5d9e86c80b9e0
James Carr <jamescarr@uk.ibm.com>
2018-01-31 18:03:23 +0000

    Fixing missing ; error in CatCon

--- src/main/java/catalog/CatalogController.java
+++ src/main/java/catalog/CatalogController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import catalog.models.*
-import org.springframework.beans.factory.annotation.Autowired
+import catalog.models.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 
